e1042b552238e93f5298ea0b40f5345e1366bb6f
Дарья <dariaabdullina000@gmail.com>
2024-12-16 14:40:49 +0300

    Add files via upload

---
+++ program06/Program.cs
@@ -0,0 +1,20 @@
+﻿// задача №17 Напишите программу, которая принимает  на вход коордитнаты точки  (х и y), 
+//причем х не = 0 и y не = 0 и выдает номер четверти плоскости,  в которой находится эта точка.
+
+string f(int x, int y)
+{
+    if (x>0 && y>0)
+    return"I";
+    else if (x<0 && y>0)
+    return"II";
+    else if (x<0 && y<0)
+    return"III";
+    return "IV";
+}
+
+
+Console.Write("Введите число: ");
+int x =  Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int y =  Convert.ToInt32(Console.ReadLine());
+Console.Write(f(x,y));
