446a30906e6606a602980427cf98d7860fc140fb
Дарья <dariaabdullina000@gmail.com>
2024-12-16 14:41:34 +0300

    Add files via upload

---
+++ programa07/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая по заданому номеру четверти,
+// показывает диапозон возможных координат точек в этой четверти ( x и y ).
+
+string f(int a)
+{
+    if (a==1)
+    return"x>0 and y>0";
+    else if (a==2)
+    return"x<0 and y>0";
+    else if (a==3)
+    return"x<0 and y<0";
+    return "x>0 and y<0";
+}
+
+
+Console.Write("Введите номер четверти: ");
+int x =  Convert.ToInt32(Console.ReadLine());
+Console.Write(f(x));
+
+
