32c233e8ca7afd1db066edf413d1d07573ff1b51
Дарья <dariaabdullina000@gmail.com>
2024-12-16 14:36:23 +0300

    Add files via upload

---
+++ program02/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задание 4: Напишите программу, которая на вход принимает число (N), а на выхоже показывает все четные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i<= n; i += 2)
+     Console.WriteLine("" + i);
+    
+
+
